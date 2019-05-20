6769306e18270e5f15b916c0c4b35c2acd8ecdfe
Patricia Falandysz <pfalandysz@yahoo.com>
2019-05-20 17:27:43 +0200

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+flats_attributes = [
+  {
+    name: 'Beautiful cottage house London',
+    address: '7 Boundary St, London E2 7JE',
+    description: 'A superbe traditional house in the middle of the city',
+    price_per_night: 55,
+    number_of_guests: 5
+  },
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'Light & Spacious Garden Flat London',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Stylish House Close to River Thames',
+    address: '5 Queensmill Road London SW6 6JP',
+    description: 'Stylish House Close to River Thames',
+    price_per_night: 100,
+    number_of_guests: 2
+  },
+  {
+    name: 'St Pancras Clock Tower Guest Suite',
+    address: 'Euston Rd London N1C 4QP',
+    description: 'A superbe traditional house in the middle of the city',
+    price_per_night: 255,
+    number_of_guests: 6
+  }
+]
+Flat.create!(flats_attributes)
